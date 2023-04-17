bc25cc0cd57eccfa69a850d8238780b88839566f
FlaShadowK <oarman93sk@gmail.com>
2023-04-18 00:13:41 +0200

    added password reset

---
+++ src/components/Auth/AuthReset.jsx
@@ -0,0 +1,33 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Auth.css";
+import AuthTitle from "./AuthTitle";
+import AuthForm from "./AuthForm";
+import AuthInput from "./AuthInput";
+import AuthButton from "./AuthButton";
+import AuthOther from "./AuthOther";
+import AuthFormContainer from "./AuthFormContainer";
+
+const AuthReset = () => {
+  return (
+      <AuthFormContainer>
+          <AuthTitle title="Pronađite svoj račun" message="Ne brinite, Vaš račun će se brzo vratiti"/>
+          <AuthForm>
+              <AuthInput
+                  title="Email"
+                  name="username"
+                  type="text"
+                  placeholder="Unesite e-mail adresu vašeg naloga"
+              />
+              <br/>
+              <AuthButton text="Dalje"/>
+              <AuthOther
+                  message="Sjetili ste se? "
+                  link="/login"
+                  linkText="Prijavite se"
+              />
+          </AuthForm>
+      </AuthFormContainer>
+  );
+}
+
+export default AuthReset;
