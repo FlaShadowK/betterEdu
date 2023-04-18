e8cf757b90bbfbd6b72621922fa362e06752a069
Alldden <aldenhasanovic@gmail.com>
2023-04-18 21:24:01 +0200

    Static bootstrap removed and customizable bootstrap added

--- src/components/Auth/AuthTitle.jsx
+++ src/components/Auth/AuthTitle.jsx
@@ -1,4 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./Auth.css";
 
 const AuthTitle = (props) => {
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,4 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import "./Auth.css";
 import AuthFormContainer from "./AuthFormContainer";
 import AuthTitle from "./AuthTitle";
