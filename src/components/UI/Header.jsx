89452cb31835849431e5dc27c099860ad94bc638
Petar Simonovic <simonovicp12@gmail.com>
2023-04-23 00:23:01 +0200

    added teaser

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -2,22 +2,35 @@ import styles from './Header.module.css';
 
 const Header = () => {
     return (
-        <nav style={{paddingLeft: '150px', paddingRight: '150px'}} className={'d-flex justify-content-between py-3 align-items-center border-bottom'}>
-            <div>
-                <a href={'/'}>
-                    <img className={'pe-4 py-3'} src="/logo.svg" alt="logo"/>
+        <>
+            <div style={{backgroundColor: "black"}}
+                 className={'d-flex justify-content-center text-center align-items-center h-100 py-3'}>
+                <a href="https://github.com/betterEdu/betterEdu">
+                    <div>
+                        <img src="https://github.githubassets.com/favicons/favicon-dark.png" alt="github"
+                             className={'pe-2'}/>
+                        <a style={{color: "white", display: "inline-block"}}>GitHUB repo</a>
+                    </div>
                 </a>
-                <span className={'align-middle ' + styles.links}>
+            </div>
+            <nav style={{paddingLeft: '150px', paddingRight: '150px'}}
+                 className={'d-flex justify-content-between py-3 align-items-center border-bottom'}>
+                <div>
+                    <a href={'/'}>
+                        <img className={'pe-4 py-3'} src="/logo.svg" alt="logo"/>
+                    </a>
+                    <span className={'align-middle ' + styles.links}>
                     <a className={'px-3'} href={'/'}>Početna</a>
                     <a className={'px-3'} href={'/'}>O nama</a>
                     <a className={'px-3'} href={'/'}>Kontakt</a>
                 </span>
-            </div>
-            <div className={styles.links}>
-                <a className={'px-4'} href={'/login'}>Prijavi se</a>
-                <a className={'btn btn-primary text-white'} href={'/register'}>Registruj se</a>
-            </div>
-        </nav>
+                </div>
+                <div className={styles.links}>
+                    <a className={'px-4'} href={'/login'}>Prijavi se</a>
+                    <a className={'btn btn-primary text-white'} href={'/register'}>Registruj se</a>
+                </div>
+            </nav>
+        </>
     );
 }
 
