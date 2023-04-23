39827bfce6321681fe3401c37d13f5a67d368593
Alden <aldenhasanovic@gmail.com>
2023-04-23 12:21:03 +0200

    Home page bootstraped

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,129 +1,115 @@
-import Header from "../UI/Header";
-import ReactDOM from "react-dom";
-import Frame from "../UI/Frame";
+import Section from "./components/Section";
+import NavBar from "./components/NavBar";
+import Header from "./components/Header";
+import Quote from "./components/Quote";
+import Footer from "./components/Footer";
 
+import './assets/main.css'
 const Home = () => {
-    return (
-        <>
-            {ReactDOM.createPortal(<Header/>, document.getElementById('header'))}
-            <Frame padding={'60px 150px'}>
-                <div className={'py-5 d-flex flex-column justify-content-center'}>
-                    <p className={'fw-bold'} style={{fontSize: '60px'}}>
-                        Za bolju, pristupačnu i jednostavnu <span className={'text-primary'}>edu</span>kaciju!
-                    </p>
-                    <p className={'fs-5'}>
-                        Naš cilj je da olakšamo posao profesorima i učenicima u školama, te da stvorimo bolje uslove
-                        za učenje i razvoj. Smatramo da je kvalitetna edukacija ključna za budućnost naše društva,
-                        te se zalažemo za stvaranje sistema koji će biti pravedniji i učinkovitiji za sve.
-                    </p>
-                    <div className={'py-3'}>
-                        <a className={'btn btn-light fw-bold'}>O nama</a>
-                        <a href={'/register'} className={'btn btn-primary fw-bold mx-3'}>Registruj se</a>
-                    </div>
-                </div>
-                <div>
-                    <div style={{padding: '80px'}}>
-                        <img src="/assets/home/icon1.svg"
-                         alt={''}/>
-                    </div>
-                </div>
-            </Frame>
+  return(
+      <div>
+          <div style={{backgroundColor: "black", gap: '20px'}}
+               className={'d-flex justify-content-center text-center align-items-center h-100 py-3'}>
+              <a target={'_blank'} href="https://github.com/betterEdu/betterEdu">
+                  <div className={'d-flex justify-content-center align-items-center'}>
+                      <div>
+                          <img src="https://github.githubassets.com/favicons/favicon-dark.png" alt="github"
+                               style={{height: '20px'}} className={'pe-2'}/>
+                      </div>
+                  </div>
+              </a>
+              <a target={'_blank'} href="https://www.figma.com/file/sVBo8tqzFELSqAMtB76fq5/hakaton?node-id=3-65&t=ISsdfaxdBPTvcG6T-0">
+                  <div className={'d-flex justify-content-center align-items-center'}>
+                      <div>
+                          <img style={{height: '20px'}} src="https://static.figma.com/app/icon/1/favicon.svg" alt="github"/>
+                      </div>
+                  </div>
+              </a>
+          </div>
+          <NavBar/>
+          <Section className={'titleHeaderSection'}>
+              <Header/>
+          </Section>
 
-            <Frame padding={'96px 100px'} bgcolor={'#F9FAFB'}>
-                <div>
-                    <img className={'d-block mx-auto mb-5'} src="/logo.svg" alt="logo"/>
-                    <p style={{fontSize: '48px'}} className={'fw-semibold'}>“Ulaganje u edukaciju je ulaganje u
-                        budućnost”</p>
-                </div>
-            </Frame>
+          <Section background={'#F9FAFB'}>
+              <Quote/>
+          </Section>
 
-            <Frame padding={'60px 150px'}>
-                <div>
-                    <div style={{padding: '80px'}}>
-                        <img src="/assets/home/icon2.svg"
-                         alt={''}/>
-                    </div>
-                </div>
-                <div className={'py-5 d-flex flex-column justify-content-center'}>
-                    <p className={'fw-bold'} style={{fontSize: '60px'}}>
-                        Edukacija
-                    </p>
-                    <p className={'fs-5'}>
-                        Edukacija je veoma bitan proces učenja i sticanja znanja, vještina i vrijednosti. U današnjem
-                        svijetu tehnologija može biti snažno sredstvo za unapređivanje edukacije na mnogo načina.
-                        Tehnologija omogućava personalizovano učenje, gdje se materijali i lekcije prilagođavaju
-                        individualnim potrebama svakog učenika.
-                    </p>
-                </div>
-            </Frame>
+          <Section>
+              <div className="container">
+                  <div className="row justify-content-between">
+                      <div className="picture1 col-md-4">
+                          <img src="/assets/home/icon2.svg" className={'img-fluid'} alt=""/>
+                      </div>
+                      <div className="d-flex textarea col-md-7 align-items-center">
+                          <div>
+                              <h1 className={'title1'}>Edukacija</h1>
+                              <p className={'para py-3'}>
+                                  Edukacija je veoma bitan proces učenja i sticanja znanja, vještina i vrijednosti. U današnjem svijetu tehnologija može biti snažno sredstvo za unapređivanje edukacije na mnogo načina. Tehnologija omogućava personalizovano učenje, gdje se materijali i lekcije prilagođavaju individualnim potrebama svakog učenika.</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </Section>
 
-            <Frame padding={'60px 150px'} bgcolor={'#F9FAFB'}>
-                <div className={'py-5 d-flex flex-column justify-content-center'}>
-                    <p className={'fw-bold'} style={{fontSize: '60px'}}>
-                        Sigurnost
-                    </p>
-                    <p className={'fs-5'}>
-                        Sigurnost je jedna od osnovnih ljudskih potreba, bez koje ne možemo normalno funkcionisati i
-                        razvijati se. Sigurnost se odnosi na zaštitu ljudi od različitih vrsta opasnosti, uključujući
-                        fizičku, emocionalnu i mentalnu. Svako nasilje predstavlja ozbiljan problem koji može imati
-                        dugoročne posljedice po žrtve i njihovo okruženje. Zbog toga je važno da se nasilje prijavi kako
-                        bi se spriječilo dalje širenje i osigurala zaštita žrtava.
-                    </p>
-                </div>
-                <div>
-                    <div style={{padding: '80px'}}>
-                        <img src="/assets/home/icon3.svg"
-                         alt={''}/>
-                    </div>
-                </div>
-            </Frame>
+          <Section background={'#F9FAFB'}>
+              <div className="container">
+                  <div className="row justify-content-between">
+                      <div className="picture1 col-md-4">
+                          <img src="/assets/home/icon3.svg" className={'img-fluid'}  alt=""/>
+                      </div>
+                      <div className="order-md-first d-flex textarea col-md-7 align-items-center">
+                          <div>
+                              <h1 className={'title1'}>Sigurnost</h1>
+                              <p className={'para py-3'}>
+                                  Sigurnost je jedna od osnovnih ljudskih potreba, bez koje ne možemo normalno funkcionisati i razvijati se. Sigurnost se odnosi na zaštitu ljudi od različitih vrsta opasnosti, uključujući fizičku, emocionalnu i mentalnu. Svako nasilje predstavlja ozbiljan problem koji može imati dugoročne posljedice po žrtve i njihovo okruženje. Zbog toga je važno da se nasilje prijavi kako bi se spriječilo dalje širenje i osigurala zaštita žrtava.
+                              </p>
+                              </div>
+                      </div>
+                  </div>
+              </div>
+          </Section>
 
-            <Frame padding={'60px 150px'}>
-                <div>
-                    <div style={{padding: '80px'}}>
-                        <img src="/assets/home/icon4.svg"
-                         alt={''}/>
-                    </div>
-                </div>
-                <div className={'py-5 d-flex flex-column justify-content-center'}>
-                    <p className={'fw-bold'} style={{fontSize: '60px'}}>
-                        Čuvanje
-                    </p>
-                    <p className={'fs-5'}>
-                        Šteta u školskim objektima može biti uzrokovana neadekvatnim održavanjem, neodgovornim
-                        ponašanjem učenika ili vanjskim faktorima, kao što su prirodne katastrofe. Spriječavanje i
-                        evidentiranje štete u školskim objektima su neophodni kako bi se osigurala sigurnost učenika i
-                        osoblja škole, kao i kako bi se sačuvala vrijednost objekta.
-                    </p>
-                </div>
-            </Frame>
+          <Section>
+              <div className="container">
+                  <div className="row justify-content-between">
+                      <div className="picture1 col-md-4">
+                          <img src="/assets/home/icon4.svg" className={'img-fluid'} alt=""/>
+                      </div>
+                      <div className="d-flex textarea col-md-7 align-items-center">
+                          <div>
+                              <h1 className={'title1'}>Čuvanje</h1>
+                              <p className={'para py-3'}>
+                                  Šteta u školskim objektima može biti uzrokovana neadekvatnim održavanjem, neodgovornim ponašanjem učenika ili vanjskim faktorima, kao što su prirodne katastrofe. Spriječavanje i evidentiranje štete u školskim objektima su neophodni kako bi se osigurala sigurnost učenika i osoblja škole, kao i kako bi se sačuvala vrijednost objekta.
+                              </p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </Section>
 
-            <Frame padding={'60px 150px'} bgcolor={'#F9FAFB'}>
-                <div className={'py-5 d-flex flex-column justify-content-center'}>
-                    <p className={'fw-bold'} style={{fontSize: '60px'}}>
-                        Aktivnost
-                    </p>
-                    <p className={'fs-5'}>
-                        Aktivnost na času je veoma važna jer omogućava učenicima da aktivno učestvuju u nastavi,
-                        učestvuju u raspravi i postavljaju pitanja. To pomaže učenicima da razumiju gradivo na većem
-                        nivou, razvijaju kritičko mišljenje i unapređuju komunikacione vještine. Vrednovanje aktivnosti
-                        učenika tokom časa može biti korisno za procjenu napretka učenika, praćenje njihovog angažmana i
-                        motivisanje za dalje učenje.
-                    </p>
-                </div>
-                <div>
-                    <div style={{padding: '80px'}}>
-                        <img src="/assets/home/icon5.svg"
-                             alt={''}/>
-                    </div>
-                </div>
-            </Frame>
+          <Section background={'#F9FAFB'}>
+              <div className="container">
+                  <div className="row justify-content-between">
+                      <div className="picture1 col-md-4">
+                          <img src="/assets/home/icon5.svg" className={'img-fluid'}  alt=""/>
+                      </div>
+                      <div className="order-md-first d-flex textarea col-md-7 align-items-center">
+                          <div>
+                              <h1 className={'title1'}>Aktivnost</h1>
+                              <p className={'para py-3'}>
+                                  Aktivnost na času je veoma važna jer omogućava učenicima da aktivno učestvuju u nastavi, učestvuju u raspravi i postavljaju pitanja. To pomaže učenicima da razumiju gradivo na većem nivou, razvijaju kritičko mišljenje i unapređuju komunikacione vještine. Vrednovanje aktivnosti učenika tokom časa može biti korisno za procjenu napretka učenika, praćenje njihovog angažmana i motivisanje za dalje učenje.
+                              </p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </Section>
 
-            <Frame padding={'36px 120px'}>
-                <p className={'fw-bold m-0'}>Made with love by better<span className={'text-primary'}>Edu</span></p>
-            </Frame>
-        </>
-    );
+          <Footer/>
+      </div>
+  );
 }
+
 
 export default Home;
---
+++ src/components/Home/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return(
+      <section style={{paddingTop: 34, paddingBottom: 34}}>
+          <div className="text-center">
+              <p style={{fontSize: 20}}>Made with love by better<span style={{color: '#0085FF'}}>Edu</span></p>
+          </div>
+      </section>
+  )
+}
+
+export default Footer;
